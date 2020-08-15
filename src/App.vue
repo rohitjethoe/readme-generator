d33a70400aeb1d203d2cf853056d1e078fda08f6
rohitjethoe <rohitchai@icloud.com>
2020-08-15 21:25:12 +0200

    Added new google fonts url.

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,8 @@ export default {
 </script>
 
 <style>
- @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap');
+ 
 * {
   margin: 0;
   padding: 0;
