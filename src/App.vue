07aab5bc463dcd680e61a7d35e3ed1ce5251e56b
rohitjethoe <rohitchai@icloud.com>
2020-08-15 20:53:54 +0200

    Added Header component.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="app">
+    <Header />
     <Create />
   </div>
 </template>
 
 <script>
+import Header from "./components/Header";
 import Create from "./components/Create";
 
 export default {
   name: 'App',
   data() {
     return {
-      title: null, subtitle: null, license: null
+      title: null, subtitle: null, license: "MIT"
     }
   },
   components: {
-    Create
+    Create,
+    Header
   }
 }
 </script>
 
 <style>
+ @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap');
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Source Sans Pro', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="header">
+        <div class="logo">
+            README Generator
+        </div>
+        <div class="subtitle">
+            Generate a Project README for your Github repo. 
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: "Header", 
+    data() {
+        return {
+            data: null
+        }
+    },
+    mounted() {
+        this.getRepoData();
+    },
+    methods: {
+        getRepoData() { 
+            axios.get('https://api.github.com/repos/rohitjethoe/readme-generator')
+            .then((res) => {
+                this.data = res.data 
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .header {
+        .logo {
+            font-size: 28px;
+            display: block;
+            text-align: center;
+            color:#85c1e9; 
+        }
+        .subtitle {
+            text-align: center;
+            font-size: 18px;
+            color: #474747;
+        }
+    }
+</style>
